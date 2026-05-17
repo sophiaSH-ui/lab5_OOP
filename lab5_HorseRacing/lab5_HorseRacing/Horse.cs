33af2d58570832196ef2d89826e020f77c8782a5
Sofia SH <shulzhenko.sofiia1124@vu.cdu.edu.ua>
2026-05-17 21:51:52 +0300

    Add Horse.cs

---
+++ lab5_HorseRacing/lab5_HorseRacing/Horse.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
+
+namespace lab5_HorseRacing
+{
+    public class Horse : INotifyPropertyChanged
+    {
+        private static readonly Random Rnd = new Random();
+        private readonly double _speed;
+
+        private string _name;
+        private Color _horseColor;
+        private TimeSpan _raceTime;
+        private double _positionX;
+        private double _coefficient;
+        private double _moneyBet;
+        private bool _isFinished;
+
+        public ImageSource SpritePlaceholder { get; set; }
+        public double Acceleration { get; private set; }
+
+        public string Name
+        {
+            get => _name;
+            set { _name = value; OnPropertyChanged(); }
+        }
+
+        public Color HorseColor
+        {
+            get => _horseColor;
+            set { _horseColor = value; OnPropertyChanged(); }
+        }
+
+        public TimeSpan RaceTime
+        {
+            get => _raceTime;
+            set { _raceTime = value; OnPropertyChanged(); }
+        }
+
+        public double PositionX
+        {
+            get => _positionX;
+            set { _positionX = value; OnPropertyChanged(); }
+        }
+
+        public double Coefficient
+        {
+            get => _coefficient;
+            set { _coefficient = value; OnPropertyChanged(); }
+        }
+
+        public double MoneyBet
+        {
+            get => _moneyBet;
+            set { _moneyBet = value; OnPropertyChanged(); }
+        }
+
+        public bool IsFinished
+        {
+            get => _isFinished;
+            set { _isFinished = value; OnPropertyChanged(); }
+        }
+
+        public Horse(string name, Color color, double coefficient)
+        {
+            Name = name;
+            HorseColor = color;
+            Coefficient = coefficient;
+            _speed = Rnd.Next(5, 11);
+            PositionX = 0;
+            IsFinished = false;
+        }
+
+        public async Task ChangeAcceleration()
+        {
+            await Task.Run(() =>
+            {
+                double multiplier = Rnd.NextDouble() * (1.0 - 0.7) + 0.7;
+                Acceleration = _speed * multiplier;
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
